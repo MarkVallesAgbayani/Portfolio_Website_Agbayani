2085039d8308b63e4b65a96860c71ea4e51868d7
MarkVallesAgbayani <markagbayanipersonal19@gmail.com>
2026-02-09 09:15:19 +0800

    Added a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="bg-white w-130 mt-10 mr-auto ml-auto rounded-full shadow-xl backdrop-blur-2xl border-1 border-gray-100 w-xs:100 sm:120 md:w-130 lg:150">
+        <nav>
+            <ul class="p-3 flex gap-3 justify-between items-center text-gray-600 font-sans text-2xl font-medium">
+                <a href="" class="hover:bg-gray-100 p-2 w-50 text-center rounded-full hover:text-gray-900 transition duration-300">
+                    <li ></li>Home</a>
+                <a href="" class="hover:bg-gray-100 p-2 w-50 text-center rounded-full hover:text-gray-900 transition duration-300">
+                    <li></li>Portfolio</a>
+                <a href="" class="hover:bg-gray-100 p-2 w-50 text-center rounded-full hover:text-gray-900 transition duration-300">
+                    <li></li>Contact</a>
+            </ul>
+        </nav>
+    </header>
+</template>
