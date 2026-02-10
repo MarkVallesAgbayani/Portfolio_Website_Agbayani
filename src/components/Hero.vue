975167175f29eeed42ffa2faaf3dcd340be43bc0
MarkVallesAgbayani <markagbayanipersonal19@gmail.com>
2026-02-10 10:06:38 +0800

    Make it mobile first responsive but not totally finished

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,46 +3,46 @@
 </script>
 
 <template>
-    <div class="h-150 flex flex-row items-center justify-around w-full">
-        <div class="p-5 flex flex-col gap-5">
-            <div class="text-8xl"> 
-                <h1 class="font-bold text-sky-900 tracking-normal">Mark Agbayani</h1>
+    <div class="md:flex md:flex-col md:justify-center items-center gap-15 h-130 md:mt-30 lg:gap-35 lg:mx-auto lg:flex lg:flex-row lg:mt-0">
+        <div class="">
+            <div class=""> 
+                <h1 class="font-bold text-sky-900 tracking-normal text-4xl mt-10 text-center mx-auto md:text-6xl lg:text-7xl">Mark Agbayani</h1>
             </div>
 
-            <div class="text-2xl w-175 text-gray-600 tracking-normal">
+            <div class="text-gray-600 tracking-normal mt-2 text-center text-[12px] w-93 mx-auto md:text-[16px]  md:w-140 md:mt-5 lg:text-[20px] lg:w-150">
                 <p>Student at STI College exploring how performance, structure, and data come together in real-world systems.</p>
             </div>
 
-            <div class="gap-3 flex">
-                <button class="bg-sky-800 shadow-2xl text-white p-5 rounded-full text-sm flex gap-2 cursor-pointer"  type="button" 
+            <div class="flex gap-2 mt-4 justify-center items-center mx-auto">
+                <button class="bg-sky-800 shadow-2xl text-white rounded-full p-2 flex gap-2 cursor-pointer text-[10px] text-center w-30 justify-center items-center md:w-30 md:h-10 lg:w-45 lg:h-16 lg:text-[14px]" type="button" 
                 >
                     View Projects 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg> 
                 </button>
-                <button class="bg-none shadow-2xl text-black p-4 w-50 rounded-full border-1 text-sm cursor-pointer" type="button"
+                <button class="bg-none shadow-2xl text-black w-40 p-2 rounded-full border-1 cursor-pointer text-[10px] md:w-30 md:h-10 lg:w-55 lg:h-16 lg:text-[14px]" type="button"
                 >
                     Download Resume
                 </button>
             </div>
         </div>
 
-        <div class="bg-white/60 w-120 h-120 rounded-4xl border-2 border-gray-50 backdrop-blur-8xl flex items-center justify-center shadow-2xl">
+        <div class="bg-white/60 w-60 h-65 rounded-2xl border-2 border-gray-50 backdrop-blur-8xl flex mb-6 items-center justify-center shadow-2xl mt-6 mr-auto ml-auto md:mx-10 md:w-120 md:h-300 p-3 lg:mx-12 lg:h-100 lg:w-100">
             <div>
                 <div>
-                    <img class="border-5 border-sky-100 rounded-full w-70 shadow-2xl" src="/public/Images/picture.png" alt="" srcset="">
+                    <img class="border-5 border-sky-100 rounded-full w-30 shadow-2xl flex justify-center items-center mt-5 mr-auto ml-auto md:w-45 lg:w-60" src="/public/Images/picture.png" alt="" srcset="">
                     <div class="text-center mt-4 flex flex-col">
-                        <p class="font-bold text-2xl tracking-normal"
+                        <p class="font-bold tracking-normal md:text-1xl lg:text-[24px]"
                         >
                         Mark Agbayani
                         
                         </p>
-                        <p class="text-sky-900 uppercase font-medium flex items-center justify-center gap-1 flex-row mt-2"
+                        <p class="text-sky-900 uppercase font-medium flex items-center justify-center gap-1 flex-row mt-2 text-[9px] md:text-[10px] lg:text-[12px]"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#0c4a6e" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pinned-icon lucide-map-pinned"><path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0"/><circle cx="12" cy="8" r="2"/><path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#0c4a6e" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pinned-icon lucide-map-pinned"><path d="M18 8c0 3.613-3.869 7.429-5.393 8.795a1 1 0 0 1-1.214 0C9.87 15.429 6 11.613 6 8a6 6 0 0 1 12 0"/><circle cx="12" cy="8" r="2"/><path d="M8.714 14h-3.71a1 1 0 0 0-.948.683l-2.004 6A1 1 0 0 0 3 22h18a1 1 0 0 0 .948-1.316l-2-6a1 1 0 0 0-.949-.684h-3.712"/></svg>
                         Quezon City</p>
-                        <div class="mt-10 flex gap-1 justify-center items-center">
-                            <p class="border-gray-400 border-1 text-sm text-center p-3 rounded-full bg-gray-100">4th Year</p>
-                            <p class="border-gray-400 border-1 text-sm text-center p-3 rounded-full bg-gray-100">Developer Intern</p>
+                        <div class="mt-3 flex gap-1 justify-center items-center">
+                            <p class="border-gray-400 border-1 text-center p-1 rounded-full bg-gray-100 text-[9px] md:text-[10px] lg:text-[12px]">4th Year</p>
+                            <p class="border-gray-400 border-1 text-center p-1 rounded-full bg-gray-100 text-[9px] md:text-[10px] lg:text-[12px]">Developer Intern</p>
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <header class="bg-white w-80 mt-5 mr-auto ml-auto rounded-full shadow-xl backdrop-blur-2xl border-1 border-gray-100 ">
+    <header class="fixed-center bg-white w-60 mt-5 mr-auto ml-auto rounded-full shadow-xl backdrop-blur-2xl border-1 border-gray-100 md:w-70 lg:w-100">
         <nav>
-            <ul class="p-1 flex gap-1 justify-between items-center text-gray-600 font-sans text-sm font-medium">
+            <ul class="p-1 flex gap-1 justify-between items-center text-gray-600 font-sans text-[10px] font-medium md:text-[14px] lg:text-[16px]">
                 <li class="hover:bg-gray-100 p-2 w-30 text-center rounded-full hover:text-gray-900 transition duration-300 cursor-pointer"> 
                     <router-link to="/">
                             Home
