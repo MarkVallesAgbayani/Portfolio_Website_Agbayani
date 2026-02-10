38c16d6adf811a5bbb548124ecce2377abe61ab7
MarkVallesAgbayani <markagbayanipersonal19@gmail.com>
2026-02-10 15:36:25 +0800

    Change the tech stack description

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ import Hero from './components/Hero.vue'
           </div>
           <p class="bg-sky-950 w-120 h-10 p-2 text-center rounded-md flex gap-3 items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layers-icon lucide-layers"><path d="M12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83z"/><path d="M2 12a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 12"/><path d="M2 17a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 17"/></svg>
-            Tech Stack: Vue.js, Vite, Tailwind CSS
+            Tech Stack: Vue.js + Vite, Tailwind CSS.
           </p>
     </div>
   </div>
